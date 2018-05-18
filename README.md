98333996795a17cb65ab08aec7041bf729904424
Sebastian Correa <scorrea92@hotmail.com>
2018-05-18 15:21:41 +0200

    coloring and styling

--- README.md
+++ README.md
@@ -5,6 +5,18 @@ Practicas desarrolladas para la asignatura Visión por Computadora del Master IA
 **Practica 1 (Gender Recognition):**
 * Implement a model with >95% accuracy over test set
 * Implement a model with >90% accuracy with less than 100K parameters
+* **[Script](https://github.com/scorrea92/computer_vision/blob/master/practica1.py)**
+
+
+**Practica 2 (Car Model identification with bi-linear models):**
+* Load a pre-trained VGG16
+* Connect this pre-trained model and form a bi-linear
+* Train freezing weights or not
+* **[Script](https://github.com/scorrea92/computer_vision/blob/master/bilinear2.ipynb)**
+
+**Practica 3 ():**
+*
+
 
 ## Built With
 
---
+++ practica1.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import keras
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Lambda
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization as BN
+from keras.layers import GaussianNoise as GN
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.models import Model
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from keras.callbacks import LearningRateScheduler as LRS
+from keras.preprocessing.image import ImageDataGenerator
+
+
+
+batch_size = 32
+num_classes = 20
+epochs = 100
+
+
+
+#### LOAD AND TRANSFORM
+
+# ## Download: ONLY ONCE!
+# !wget https://www.dropbox.com/s/kdhn10jwj99xkv7/data.tgz
+# !tar xvzf data.tgz
+# #####
+
+
+# Load 
+x_train = np.load('x_train.npy')
+x_test = np.load('x_test.npy')
+
+y_train = np.load('y_train.npy')
+y_test = np.load('y_test.npy')
+
+# Stats
+print(x_train.shape)
+print(y_train.shape)
+
+print(x_test.shape)
+print(y_test.shape)
+
+## View some images
+# plt.imshow(x_train[2,:,:,: ] )
+# plt.show()
+
+
+## Transforms
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+y_train = y_train.astype('float32')
+y_test = y_test.astype('float32')
+
+
+x_train /= 255
+x_test /= 255
+
+
+## Labels
+y_train=y_train-1
+
+y_test=y_test-1
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+###########################################################
+
+from keras.applications.vgg16 import VGG16
+
+# load the model
+model1 = VGG16(weights='imagenet', include_top=False, input_shape=x_train.shape[1:])
+
+#############################
+###      BILINEAR        ####
+#############################
+
+for layer in model1.layers:
+    layer.trainable = False
+
+def outer_product(x):
+  phi_I = tf.einsum('ijkm,ijkn->imn',x[0],x[1])		# Einstein Notation  [batch,1,1,depth] x [batch,1,1,depth] -> [batch,depth,depth]
+  phi_I = tf.reshape(phi_I,[-1,512*512])	        # Reshape from [batch_size,depth,depth] to [batch_size, depth*depth]
+  phi_I = tf.divide(phi_I,31*31)								  # Divide by feature map size [sizexsize]
+
+  y_ssqrt = tf.multiply(tf.sign(phi_I),tf.sqrt(tf.abs(phi_I)+1e-12))		# Take signed square root of phi_I
+  z_l2 = tf.nn.l2_normalize(y_ssqrt, dim=1)								              # Apply l2 normalization
+  return z_l2
+
+
+
+conv=model1.get_layer('block4_conv3') # block4_conv3
+d1=Dropout(0.5)(conv.output)   ## Why??
+d2=Dropout(0.5)(conv.output)   ## Why??
+
+x = Lambda(outer_product, name='outer_product')([d1,d2])
+
+predictions=Dense(num_classes, activation='softmax', name='predictions')(x)
+
+model = Model(inputs=model1.input, outputs=predictions)
+
+
+# DEFINE A LEARNING RATE SCHEDULER
+def scheduler(epoch):
+    if epoch < 25:
+        return .1
+    elif epoch < 50:
+        return 0.01
+    else:
+        return 0.001
+
+set_lr = LRS(scheduler)
+
+
+## DATAGEN
+datagen = ImageDataGenerator(
+  width_shift_range=0.2,
+  height_shift_range=0.2,
+  rotation_range=20,
+  zoom_range=[1.0,1.2],
+  horizontal_flip=True)
+
+
+## OPTIM AND COMPILE use Adam Rsmprop
+opt = SGD(lr=0.0001, decay=1e-6)
+RMSprop(lr=0.0001, rho=0.9, epsilon=None, decay=0.0)
+adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-6, amsgrad=False)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=adam,
+              metrics=['accuracy'])
+    
+  
+model.summary()
+
+
+
+## TRAINING with DA and LRA
+history=model.fit_generator(datagen.flow(x_train, y_train,batch_size=batch_size),
+                            steps_per_epoch=len(x_train) / batch_size, 
+                            epochs=epochs,
+                            validation_data=(x_test, y_test),
+                            verbose=1)
+
+#Usar Checpoint mejor de ejecución
+#for layer in model1.layers:
+    #if j <14:
+        #if "conv" in layer.name:
+            #layer.trainable = False
+            
+#Compile y ejecutar de nuevo
